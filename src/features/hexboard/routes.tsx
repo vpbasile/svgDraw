289c48564a61611baf17575cb9bb21f2ee85b909
Vincent Basile <vpbphone@gmail.com>
2026-01-19 20:53:40 -0500

    Simplify routing for hexboards

--- src/features/hexboard/routes.tsx
+++ src/features/hexboard/routes.tsx
@@ -1,11 +1,28 @@
+import { Grid } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import CandyLand from "./boards/candyLand";
+import Cataan from "./boards/cataan";
+import CreateBoard from "./boards/createBoard";
+import GenerativeBoard from "./boards/generative";
+import SavedBoard from "./boards/hex-SavedBoard";
+import TriviaBoard from "./boards/TriviaBoard";
 import HexBoardIndex from "./HexBoardIndex";
-import { hexBoardList } from "./HexBoardList";
 
 interface FeatureRoute {
   path: string;
   element: ReactNode;
 }
+
+const hexBoardList = [
+  { uid: 'trivia', displayName: 'Trivia Board', element: <TriviaBoard /> },
+  { uid: 'saved', displayName: 'Saved Board', element: <SavedBoard /> },
+  { uid: 'create', displayName: 'Create Board', element: <CreateBoard /> },
+  { uid: 'generative', displayName: 'Generative Board', element: <GenerativeBoard /> },
+  { uid: 'candy', displayName: 'Candy Land', element: <CandyLand /> },
+  { uid: 'cataan', displayName: 'Cataan', element: <Cataan /> },
+  { uid: 'grid', displayName: 'Grid', element: <Grid /> },
+  // Other boards can be added here...
+];
 
 export const hexboardRoutes: FeatureRoute[] = [
   { path: '', element: <HexBoardIndex /> },
